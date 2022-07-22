34923c5dd1cd64ff87b0cb21cb1fa9b118892dff
yassinzouari <77297425+yassinzouari@users.noreply.github.com>
2022-07-22 11:17:44 +0100

    disable csrf

--- src/main/java/tn/esprit/banque/security/SecurityConfig.java
+++ src/main/java/tn/esprit/banque/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
 	
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
             .authorizeHttpRequests((authz) -> authz
                 .anyRequest().authenticated()
             )
