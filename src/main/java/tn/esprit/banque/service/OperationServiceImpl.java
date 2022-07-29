1920a57aa22efa20e5d994678974e7b88177fd5c
chikhaoui01 <achref.chikhaoui@esprit.tn>
2022-07-29 22:06:41 +0100

    operation crud + relevé bancaire

--- src/main/java/tn/esprit/banque/service/OperationServiceImpl.java
+++ src/main/java/tn/esprit/banque/service/OperationServiceImpl.java
@@ -4,35 +4,52 @@ import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import tn.esprit.banque.exceptions.InvalidAccountException;
+import tn.esprit.banque.model.Carte;
 import tn.esprit.banque.model.Compte;
 import tn.esprit.banque.model.Operation;
+import tn.esprit.banque.model.RemiseCheque;
 import tn.esprit.banque.model.Retrait;
 import tn.esprit.banque.model.Versement;
+import tn.esprit.banque.model.Virement;
+import tn.esprit.banque.repository.CarteRepository;
 import tn.esprit.banque.repository.CompteRepository;
 import tn.esprit.banque.repository.OperationRepository;
 import tn.esprit.banque.service.compte.CompteContrat;
+import tn.esprit.banque.service.compte.CompteCourant;
 @Service
 public class OperationServiceImpl implements OperationService {
-	@Autowired
-	private CompteRepository compteRepository;
+	
 	@Autowired
 	private OperationRepository operationRepository;
-	 private CompteContrat CompteService;
-
-@Override
+	@Autowired
+	private CompteContrat CompteService;
+	
+	private CompteRepository compteRepo;
+    @Autowired
+    public void setCompteRepo(CompteRepository compteRepo) {
+        this.compteRepo = compteRepo;
+    }
+    @Transactional
+    @Override
 	public void verser(Long idCpte, BigDecimal montant,String commentaire) {
-		Compte cp = null;
+		
+		
+		Compte cp= new Compte();
 		try {
+			//Compte cp1 = compteRepo.findById(idCpte).orElseThrow(() -> new InvalidAccountException("Compte indisponible"));
 			cp = CompteService.findLeCompte(idCpte);
 		} catch (InvalidAccountException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		Date date =  new Date();
 		Calendar c = Calendar.getInstance(); 
 		c.setTime(date); 
@@ -40,11 +57,13 @@ public class OperationServiceImpl implements OperationService {
 		date = c.getTime();
 		
 		Versement v = new Versement( new Date(), date,"validate" , commentaire, cp);
+		v.setMontant(montant.longValue());
 		cp.setSoldeCompte(cp.getSoldeCompte().add(montant));
-		compteRepository.save(cp);
+		operationRepository.save(v);
+		compteRepo.save(cp);
 		
 	}
-
+    @Transactional
 	@Override
 	public void retirer(Long idCpte, BigDecimal montant, String commentaire) {
 		Compte cp = null;
@@ -54,7 +73,7 @@ public class OperationServiceImpl implements OperationService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		BigDecimal rouge = null;
+		//BigDecimal rouge = cp.get
 		BigDecimal solde =cp.getSoldeCompte();
 		Date date =  new Date();
 		Calendar c = Calendar.getInstance(); 
@@ -62,36 +81,61 @@ public class OperationServiceImpl implements OperationService {
 		c.add(Calendar.DATE, 1);
 		date = c.getTime();
 		//if compteCourant
-		if(true) {
-			rouge= rouge.add(new BigDecimal(50));
-		}
-		if( solde.add(rouge).compareTo(montant) >1)
+		
+		if( solde.compareTo(montant) >1)
 			throw new RuntimeException("Solde insuffisant");
 		Retrait r = new Retrait(new Date(), date,"validate" , commentaire, cp);
+		r.setMontant(montant.longValue());
 		cp.setSoldeCompte(cp.getSoldeCompte().subtract(montant) );
-		compteRepository.save(cp);
+		operationRepository.save(r);
+		compteRepo.save(cp);
 		
 	}
-
+   
 	@Override
 	public void virement(Long cpt1, Long cpt2, BigDecimal montant, String Commentaire) {
 		if(cpt1.equals(cpt2)){
 			throw new RuntimeException("Impossibile de faire un virement sur le même compte");
 		}
 //		
-		retirer(cpt1,montant,"");
-		verser(cpt2, montant,"");
+		Compte cp1= new Compte();
+		Compte cp2= new Compte();
+		try {
+			//Compte cp1 = compteRepo.findById(idCpte).orElseThrow(() -> new InvalidAccountException("Compte indisponible"));
+			cp1 = CompteService.findLeCompte(cpt1);
+			cp2 = CompteService.findLeCompte(cpt2);
+			
+		} catch (InvalidAccountException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Date date =  new Date();
+		Calendar c = Calendar.getInstance(); 
+		c.setTime(date); 
+		c.add(Calendar.DATE, 1);
+		date = c.getTime();
+		if( cp1.getSoldeCompte().compareTo(montant) >1)
+			throw new RuntimeException("Solde insuffisant");
+		Virement v = new Virement(new Date(), date, "validate",Commentaire , cp1);
+		v.setMontant(montant.longValue());
+		cp1.setSoldeCompte(cp1.getSoldeCompte().subtract(montant) );
+		cp2.setSoldeCompte(cp2.getSoldeCompte().add(montant));
+		operationRepository.save(v);
+		compteRepo.save(cp1);
+		compteRepo.save(cp2);
+		
+		
+		
+		
+		
+		
 		
 	}
 
-	/*@Override
-	public List<Operation> listOperation(Long cpt) {
-		
-		return (List<Operation>) operationRepository.listOperation(cpt);
-	}
-*/
+	
 	@Override
-	public void remiseCheque(Long cpt1, Long cpt2, BigDecimal montant, String Commentaire, Long numCheque) {
+	public void remiseCheque(Long cpt1, Long cpt2, BigDecimal montant, String commentaire, Long numCheque) {
 		Compte cp = null;
 		Compte cp2 = null;
      	try {
@@ -102,19 +146,30 @@ public class OperationServiceImpl implements OperationService {
 			e.printStackTrace();
 		}
      	
-     	if (cp.getSoldeCompte().compareTo(montant)==1 ||cp.getSoldeCompte().compareTo(montant)==0 )
-		virement(cpt1, cpt2, montant, Commentaire);
-     	else 
-     		//compte compteCourant
-     		if (false)
-     		throw new RuntimeException("Impossibile de verser le cheque numéro "+numCheque +" dans le compte" +cpt2);
+     	if (cp.getSoldeCompte().compareTo(montant)==1 ||cp.getSoldeCompte().compareTo(montant)==0 ) {
+     		
+     		Date date =  new Date();
+    		Calendar c = Calendar.getInstance(); 
+    		c.setTime(date); 
+    		c.add(Calendar.DATE, 1);
+    		date = c.getTime();
+    		if( cp.getSoldeCompte().compareTo(montant) >1)
+    			throw new RuntimeException("Solde insuffisant");
+    		RemiseCheque r = new RemiseCheque(new Date(), date, "validate", commentaire, cp);
+    			//	RemiseCheque(cp, cp2, montant, Commentaire, numCheque); 
+    		r.setMontant(montant.longValue());
+    		cp.setSoldeCompte(cp.getSoldeCompte().subtract(montant) );
+    		cp2.setSoldeCompte(cp2.getSoldeCompte().add(montant));
+    		operationRepository.save(r);
+    		compteRepo.save(cp);
+    		compteRepo.save(cp2);
+     	}
+		
+     	
 	}
 
-	@Override
-	public List<Operation> listOperation(Long cpt) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+	
 
 
 	
